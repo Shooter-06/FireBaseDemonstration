0cea39ba69f61d18be0659b5776d760c5c9aa66c
Shooter-06 <john190curry@gmail.com>
2022-11-07 15:45:56 -0500

    fire base fetch

--- app/src/main/java/com/example/firebasedemonstrationnov7/MainActivity.java
+++ app/src/main/java/com/example/firebasedemonstrationnov7/MainActivity.java
@@ -1,30 +1,42 @@
 package com.example.firebasedemonstrationnov7;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.renderscript.Sampler;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextClock;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     EditText name, age, phone, height;
-
-    Button save;
+    TextView nameT, ageT, phoneT, heightT;
+    Button save, fetch;
     DatabaseReference reff;
     Member member  ;
 
     long maxid = 0;
+
+    ListView lv;
+    ArrayList<String> arrayList = new ArrayList<>();
+    ArrayAdapter<String> arrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +48,19 @@ public class MainActivity extends AppCompatActivity {
         phone= findViewById(R.id.phone);
         height= findViewById(R.id.height);
 
-        save= findViewById(R.id.save);
+        nameT= findViewById(R.id.name);
+        ageT= findViewById(R.id.age);
+        phoneT= findViewById(R.id.phone);
+        heightT= findViewById(R.id.height);
 
+        save= findViewById(R.id.save);
+        fetch= findViewById(R.id.fetch);
         member= new Member();
+
+        lv= findViewById(R.id.listView);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        lv.setAdapter(arrayAdapter);
+
 
         reff= FirebaseDatabase.getInstance().getReference().child("Member");
 
@@ -74,5 +96,57 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        fetch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reff = FirebaseDatabase.getInstance().getReference("Member");
+                reff.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+
+                        String value = snapshot.getValue(Member.class).toString();
+                        arrayList.add(value);
+                        arrayAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+//                reff = FirebaseDatabase.getInstance().getReference().child("Member").child("3");
+//                reff.addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                        String name = snapshot.child("name").getValue().toString();
+//                        String age = snapshot.child("age").getValue().toString();
+//                        String phone = snapshot.child("phone").getValue().toString();
+//                        String height = snapshot.child("height").getValue().toString();
+//
+//                        nameT.setText(name);
+//                        ageT.setText(age);
+//                        phoneT.setText(phone);
+//                        heightT.setText(height);
+//
+//
+//                    }
+                });
+            }
+        });
     }
 }
