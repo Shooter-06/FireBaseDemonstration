84346e7c70c27b0ba5367335c7403b8bdcf1e0a9
Shooter-06 <john190curry@gmail.com>
2022-11-07 14:29:34 -0500

    reff new id counter

--- app/src/main/java/com/example/firebasedemonstrationnov7/MainActivity.java
+++ app/src/main/java/com/example/firebasedemonstrationnov7/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.firebasedemonstrationnov7;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,8 +11,11 @@ import android.widget.TextClock;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
     EditText name, age, phone, height;
@@ -19,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     Button save;
     DatabaseReference reff;
     Member member  ;
+
+    long maxid = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +42,20 @@ public class MainActivity extends AppCompatActivity {
 
         reff= FirebaseDatabase.getInstance().getReference().child("Member");
 
+        reff.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    maxid= (snapshot.getChildrenCount());
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,8 +67,8 @@ public class MainActivity extends AppCompatActivity {
                 member.setAge(ageEmployee);
                 member.setHeight(height);
                 member.setPhone(ph);
-
-                reff.push().setValue(member);
+                //reff.push().setValue(member);
+                reff.child(String.valueOf(maxid + 1)).setValue(member);
                 Toast.makeText(getApplicationContext(), "Data is inserted", Toast.LENGTH_LONG).show();
 
             }
